a984e750dad661e87537bd3febaf02632b70869e
Stanislav Platov <pllattoff@gmail.com>
2026-09-09 17:33:48 +0200

    add OAuth2

---
+++ backend/src/main/java/org/restaurantfinder/backend/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.restaurantfinder.backend.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(o -> o
+                        .defaultSuccessUrl(frontendUrl));
+
+        return http.build();
+    }
+}
