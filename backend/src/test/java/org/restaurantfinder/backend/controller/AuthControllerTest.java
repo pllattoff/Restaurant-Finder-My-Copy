76281c8ce693ee5a8b9b52711e02341777cf81c2
Stanislav Platov <pllattoff@gmail.com>
2026-09-10 18:21:26 +0200

    add oidcLogin() in AuthControllerTest

---
+++ backend/src/test/java/org/restaurantfinder/backend/controller/AuthControllerTest.java
@@ -0,0 +1,31 @@
+package org.restaurantfinder.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getMe() throws Exception {
+        mockMvc.perform(get("/api/auth/me")
+                        .with(oidcLogin()
+                                .userInfoToken(token -> token
+                                        .claim("login", "testUser"))
+                        ))
+                .andExpect(status().isOk())
+                .andExpect(content().string("testUser"));
+    }
+}
